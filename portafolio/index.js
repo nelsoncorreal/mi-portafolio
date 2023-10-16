6b5279f9fd6fd115e4cf369fb8694a2e9ed55c64
nelsoncorreal <55365292+nelsoncorreal@users.noreply.github.com>
2023-10-16 15:57:00 -0500

    Update index.js

--- portafolio/index.js
+++ portafolio/index.js
@@ -1,4 +1,4 @@
-let likeCounts = {};
+let likeCounts = JSON.parse(localStorage.getItem("likeCounts")) || {};
 
 function addLike(projectId) {
     if (!likeCounts[projectId]) {
@@ -6,4 +6,14 @@ function addLike(projectId) {
     }
     likeCounts[projectId]++;
     document.getElementById(`like-count-${projectId}`).textContent = likeCounts[projectId];
+
+    // Guarda el conteo actual de likes en el localStorage
+    localStorage.setItem("likeCounts", JSON.stringify(likeCounts));
 }
+
+// Inicialización: carga el conteo de likes del localStorage al cargar la página
+document.addEventListener("DOMContentLoaded", function() {
+    for (let projectId in likeCounts) {
+        document.getElementById(`like-count-${projectId}`).textContent = likeCounts[projectId];
+    }
+});
