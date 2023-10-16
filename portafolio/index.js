4226739baf60bb947ed05dd60537eab915c517c3
nelsoncorreal <55365292+nelsoncorreal@users.noreply.github.com>
2023-10-16 18:07:54 -0500

    Update index.js

--- portafolio/index.js
+++ portafolio/index.js
@@ -1,19 +1,62 @@
-let likeCounts = JSON.parse(localStorage.getItem("likeCounts")) || {};
+document.addEventListener('DOMContentLoaded', (event) => {
+    loadLikes('calculator');
+    loadLikes('clock');
+});
 
-function addLike(projectId) {
-    if (!likeCounts[projectId]) {
-        likeCounts[projectId] = 0;
-    }
-    likeCounts[projectId]++;
-    document.getElementById(`like-count-${projectId}`).textContent = likeCounts[projectId];
-
-    // Guarda el conteo actual de likes en el localStorage
-    localStorage.setItem("likeCounts", JSON.stringify(likeCounts));
+function loadLikes(type) {
+    const storedLikes = localStorage.getItem(type + 'Likes');
+    const likeCount = document.getElementById('likeCount' + capitalize(type));
+    likeCount.textContent = storedLikes ? storedLikes : 0;
 }
 
-// Inicialización: carga el conteo de likes del localStorage al cargar la página
-document.addEventListener("DOMContentLoaded", function() {
-    for (let projectId in likeCounts) {
-        document.getElementById(`like-count-${projectId}`).textContent = likeCounts[projectId];
-    }
-});
+function addLike(type) {
+    const likeCount = document.getElementById('likeCount' + capitalize(type));
+    let currentLikes = parseInt(likeCount.textContent);
+    currentLikes++;
+    likeCount.textContent = currentLikes;
+    localStorage.setItem(type + 'Likes', currentLikes);
+}
+
+function capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+}
+
+
+
+function updateClock() {
+    const now = new Date();
+
+    // Actualizar reloj digital
+    const digitalTime = now.toLocaleTimeString();
+    document.getElementById('digital-time').textContent = digitalTime;
+
+    // Actualizar reloj análogo
+    const seconds = now.getSeconds();
+    const minutes = now.getMinutes();
+    const hours = now.getHours();
+
+    const secondsDegree = ((seconds / 60) * 360) + 90;
+    const minutesDegree = ((minutes / 60) * 360) + 90;
+    const hoursDegree = ((hours / 12) * 360) + 90;
+
+    document.getElementById('second-hand').style.transform = `rotate(${secondsDegree}deg)`;
+    document.getElementById('minute-hand').style.transform = `rotate(${minutesDegree}deg)`;
+    document.getElementById('hour-hand').style.transform = `rotate(${hoursDegree}deg)`;
+}
+
+// Actualizar el reloj cada segundo
+setInterval(updateClock, 1000);
+
+function display(val) {
+    document.getElementById("result").value += val;
+}
+
+function calculate() {
+    let x = document.getElementById("result").value;
+    let y = eval(x);
+    document.getElementById("result").value = y;
+}
+
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
